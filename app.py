330925fac252785821b934e343b065217c149fd2
Ramona000 <ramonahofer@hispeed.ch>
2026-04-15 10:46:39 +0200

    Unterseite hinzugefügt

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default
 pg_second = st.Page("views/pH_Werte_Rechner.py", title="pH-Rechner", icon=":material/info:")
 pg_third = st.Page("views/Einheitenumrechner.py", title="Einheitenumrechner", icon=":material/info:")
 
-pg = st.navigation([pg_home, pg_second])
+pg = st.navigation([pg_home, pg_second, pg_third])
 pg.run()
