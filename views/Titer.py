e9a27f5cd34d1b56518e14d6b17444ca00127361
Ramona000 <ramonahofer@hispeed.ch>
2026-04-21 18:10:53 +0200

    Titer Rechner codiert

---
+++ views/Titer.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.title("Titer-Rechner")
+
+c_soll = st.number_input("Soll-Konzentration (mol/L)", min_value=0.0, format="%.6f")
+c_eff = st.number_input("Effektive Konzentration (mol/L)", min_value=0.0, format="%.6f")
+
+
+def berechne_titer(c_soll, c_eff):
+    if c_eff == 0:
+        raise ValueError("Die effektive Konzentration darf nicht 0 sein.")
+    
+    titer = c_eff / c_soll
+    return titer
+
+if st.button("Titer berechnen"):
+    try:
+        titer = berechne_titer(c_soll, c_eff)
+        st.success(f"Der berechnete Titer beträgt: {titer:.6f}")
+    except ValueError as e:
+        st.error(str(e))
+
+#Interpretation
+    if titer > 1:
+        st.success("Lösung ist stärker als erwartet")
+    elif titer < 1:
+        st.warning("Lösung ist schwächer als erwartet")
+    else:
+        st.info("Lösung ist genau richtig")
