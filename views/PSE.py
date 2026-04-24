e8b3c82df79a0e319621c4239174442859d88ea6
serwinze <winzeser@students.zhaw.ch>
2026-04-24 14:48:46 +0200

    PSE

--- views/PSE.py
+++ views/PSE.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.title("Periodensystem")
 
-st.image("views/assets/periodensystem.png", caption="Periodensystem der Elemente")
+html_code = """
+<iframe src="https://ptable.com/#Properties"
+width="100%" height="800"></iframe>
+"""
+
+components.html(html_code, height=800)
